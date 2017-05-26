b432ebf693837aaecb779532f8920443688ca0c5
lizziekarr <lizziemoves@gmail.com>
2017-05-26 00:08:20 -0500

    fix bugs

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ $(window).scroll(function(){
 
   var wScroll = $(this).scrollTop();
 
+  $('.pic-two').css({
+    'transform': 'translate(-'+ wScroll /20 +'%, 0)'
+  });
+
+  $('.pic-three').css({
+    'transform': 'translate('+ wScroll/10 +'%, '+ wScroll /10+'%)'
+  });
+
   if(wScroll > $('.cartoons').offset().top - ($(window).height() / 1.2)) {
     $('.cartoons figure').each(function(i){
       setTimeout(function() {
@@ -20,38 +28,57 @@ $(window).scroll(function(){
 $('.eat').on('click', function(){
   $('.crying').fadeOut();
   $('.snacks').fadeIn();
+  $('.is-goat').fadeOut();
+  $('.creature').removeClass('active');
+  $(this).addClass('active');
 });
 
 $('.cry').on('click', function(){
   $('.snacks').fadeOut();
   $('.crying').fadeIn();
+  $('.is-goat').fadeOut();
+  $('.creature').removeClass('active');
+  $(this).addClass('active');
 });
 $('.goat').on('click', function(){
   $('.snacks').fadeOut();
   $('.crying').fadeOut();
   $('.is-goat').fadeIn();
+  $('.creature').removeClass('active');
+  $(this).addClass('active');
 });
 
 $('.sit').on('click', function(){
   $('.cartoons img').removeClass('is-framed');
   $('.cartoons img').removeClass('is-flipping');
   $('.cartoons').removeClass('is-disco');
+  $('.cartoons img').removeClass('is-shrinking');
+  $('.do').removeClass('active');
+  $(this).addClass('active');
 });
 
 $('.flip').on('click', function(){
   $('.cartoons img').toggleClass('is-flipping');
-});
-
-$('.ferris').on('click', function(){
-  $('.cartoons').toggleClass('is-ferris');
+  $('.sit').removeClass('active');
+  $(this).toggleClass('active');
 });
 
 $('.disco').on('click', function(){
   $('.cartoons').toggleClass('is-disco');
+  $('.sit').removeClass('active');
+  $(this).toggleClass('active');
 });
 
 $('.framed').on('click', function(){
   $('.cartoons img').toggleClass('is-framed');
+  $('.sit').removeClass('active');
+  $(this).toggleClass('active');
+});
+
+$('.shrink').on('click', function(){
+  $('.cartoons img').toggleClass('is-shrinking');
+  $('.sit').removeClass('active');
+  $(this).toggleClass('active');
 });
 
 
